1778e622f7d326f4ae663f7207a3d127205e9684
Hemal Patel <95436169+HemalSPatel@users.noreply.github.com>
2023-03-27 14:13:19 -0400

    Initial commit

---
+++ src/main/java/com/example/rucafe/project4sm/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.rucafe.project4sm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.rucafe.project4sm {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.rucafe.project4sm to javafx.fxml;
+    exports com.example.rucafe.project4sm;
+}
