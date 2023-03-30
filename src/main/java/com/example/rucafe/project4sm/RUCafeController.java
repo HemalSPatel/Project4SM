3a4efa7eb1af571f06c1e103a3bc44e3be53608d
Hemal Patel <95436169+HemalSPatel@users.noreply.github.com>
2023-03-30 11:28:33 -0400

    Push

--- src/main/java/com/example/rucafe/project4sm/RUCafeController.java
+++ src/main/java/com/example/rucafe/project4sm/RUCafeController.java
@@ -8,27 +8,65 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class RUCafeController {
+
     @FXML
     private Button order_donut;
+    private Button order_coffee;
+    private Button your_order;
+    private Button store_order;
 
     @FXML
     private void onOrderDonutClick(ActionEvent event) throws IOException {
-        // Load the FXML file for the second view
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("donut-view.fxml"));
-        Parent secondViewParent = loader.load();
+        Parent root = FXMLLoader.load(getClass().getResource ("donut-view.fxml"));
+        Scene scene = new Scene (root);
+        Stage primaryStage = new Stage ();
+        primaryStage.setTitle("Order Donut");
+        primaryStage.setScene (scene);
 
-        // Create a new scene with the second view and set it on the stage
-        Scene secondViewScene = new Scene(secondViewParent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(secondViewScene);
-
-        // Get the controller for the second view and initialize it
-        DonutController controller = loader.getController();
-        controller.initialize();
+        primaryStage.initModality (Modality.APPLICATION_MODAL);
+        primaryStage.show();
     }
+
+    @FXML
+    private void onOrderCoffeeClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource ("coffee-view.fxml"));
+        Scene scene = new Scene (root);
+        Stage primaryStage = new Stage ();
+        primaryStage.setTitle("Order Coffee");
+        primaryStage.setScene (scene);
+
+        primaryStage.initModality (Modality.APPLICATION_MODAL);
+        primaryStage.show();
+    }
+
+    @FXML
+    private void onYourOrderClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource ("basket-view.fxml"));
+        Scene scene = new Scene (root);
+        Stage primaryStage = new Stage ();
+        primaryStage.setTitle("Your Order");
+        primaryStage.setScene (scene);
+
+        primaryStage.initModality (Modality.APPLICATION_MODAL);
+        primaryStage.show();
+    }
+
+    @FXML
+    private void onStoreOrderClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource ("store-order-view.fxml"));
+        Scene scene = new Scene (root);
+        Stage primaryStage = new Stage ();
+        primaryStage.setTitle("Store Orders");
+        primaryStage.setScene (scene);
+
+        primaryStage.initModality (Modality.APPLICATION_MODAL); // default
+        primaryStage.show();
+    }
+    // when placing an order you and put up a alert window to notify the user that their order has been placed
 }
