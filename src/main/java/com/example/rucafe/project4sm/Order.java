aafcd3de9ecca67281bc420b6c9b58501f6e40b5
pillow3 <ishika21patel@gmail.com>
2023-04-01 15:50:07 -0400

    added comments

--- src/main/java/com/example/rucafe/project4sm/Order.java
+++ src/main/java/com/example/rucafe/project4sm/Order.java
@@ -3,22 +3,39 @@ package com.example.rucafe.project4sm;
 import java.util.ArrayList;
 
 public class Order {
+
     private int orderNumber;
     private ArrayList<MenuItem> items;
 
+    /**
+     * constructor for an order object
+     * @param orderNumber
+     */
     public Order(int orderNumber) {
         this.orderNumber = orderNumber;
         this.items = new ArrayList<>();
     }
 
+    /**
+     * appends a menu item to the order list
+     * @param item
+     */
     public void addItem(MenuItem item) {
         items.add(item);
     }
 
+    /**
+     * removes a menu item from the order list
+     * @param item
+     */
     public void removeItem(MenuItem item) {
         items.remove(item);
     }
 
+    /**
+     * calculates the running total for all of the menu items in the order
+     * @return total price of items
+     */
     public double getOrderTotal() {
         double total = 0.0;
         for (MenuItem item : items) {
@@ -27,10 +44,18 @@ public class Order {
         return total;
     }
 
+    /**
+     * getter method for the private instance variable order number
+     * @return order number
+     */
     public int getOrderNumber() {
         return orderNumber;
     }
 
+    /**
+     * getter method for all the items in the order
+     * @return an array list of all the menu items
+     */
     public ArrayList<MenuItem> getItems() {
         return items;
     }
